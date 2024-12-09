a90ddb322cd5ffa1d0fae45dd23c72b558a06712
Robin Christ <robin@rchrist.io>
2024-12-09 13:08:48 +0100

    Adjust conanfile

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -1,13 +1,16 @@
+from conan import ConanFile
+from conan.tools.build import can_run
+from conan.tools.cmake import cmake_layout, CMake
 import os
 
-from conan import ConanFile
-from conan.tools.cmake import CMake, cmake_layout
-from conan.tools.build import can_run
 
+class TestPackageConan(ConanFile):
+    settings = "os", "arch", "compiler", "build_type"
+    generators = "CMakeDeps", "CMakeToolchain", "VirtualRunEnv"
+    test_type = "explicit"
 
-class otfftppTestConan(ConanFile):
-    settings = "os", "compiler", "build_type", "arch"
-    generators = "CMakeDeps", "CMakeToolchain"
+    def layout(self):
+        cmake_layout(self)
 
     def requirements(self):
         self.requires(self.tested_reference_str)
@@ -17,10 +20,7 @@ class otfftppTestConan(ConanFile):
         cmake.configure()
         cmake.build()
 
-    def layout(self):
-        cmake_layout(self)
-
     def test(self):
         if can_run(self):
-            cmd = os.path.join(self.cpp.build.bindir, "otfftpp_test_package")
-            self.run(cmd, env="conanrun")
+            bin_path = os.path.join(self.cpp.build.bindir, "test_package")
+            self.run(bin_path, env="conanrun")
